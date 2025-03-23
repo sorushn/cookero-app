dba17b35fa50e39d67b4b2b65590a1960f057366
Soroush Nikjou <sorushn@gmail.com>
2025-03-23 14:20:12 +0100

    mount recipe router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from backend.app.api.routes.auth import auth_router
+from backend.app.api.routes.recipe import recipe_router
 
 app = FastAPI(title="Cookero Backend API", description="The backend API for Cookero", version="0.1.0")
 
 app.include_router(auth_router, prefix="/auth")
+app.include_router(recipe_router)
